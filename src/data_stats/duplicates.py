d7129edbf418b810ad9be98e5b2cf0075df3bf88
Jevgeni Menšenin <jevgeni.mensenin@gmail.com>
2017-04-08 11:35:44 +0300

    duplicates stats

--- src/data_stats/duplicates.py
+++ src/data_stats/duplicates.py
@@ -8,12 +8,24 @@ def get_cramers_by_partition(dataframe):
     column_start = 1
     column_end = 46
     for i in range(1, 11):
-        print('{}-{}'.format(column_start,column_end))
+        print('{}-{}'.format(column_start, column_end))
         ctx_duplicate_pairs = cramers_v.get_column_pairs_by_cramers_coef_opt(dataframe.iloc[:, column_start:column_end], 1, 1, True)
         np.savetxt(constants.DATA + 'cramers_out/cramers_v_{}_{}.txt'.format(column_start, column_end), delimiter=',', fmt="%s",
                    X=ctx_duplicate_pairs)
         column_start += 46
         column_end += 46
+
+
+def count_ctx_duplicates_appearance(dataframe):
+    checked = []
+    result = []
+    for column in dataframe:
+        for value in dataframe[[column]].values:
+            if value not in checked:
+                times = (dataframe == value).sum().sum()
+                result.append([value[0], times])
+                checked.append(value)
+    return result
 
 
 # df = data_util.get_dataframe(constants.DATA + 'dates_0_10_as_bool_150.csv')
@@ -23,4 +35,9 @@ def get_cramers_by_partition(dataframe):
 # df_no_complete_duplicates.T.to_csv(constants.CLUSTER_OUT + 'no_complete_duplicates.csv', index=False, header=False, encoding='UTF-8')
 # print('complete duplicates: {}/{}'.format(complete_duplicates.sum(), df.shape[1]))
 no_ctx_duplicates = data_util.get_dataframe(constants.DATA + 'cramers_out/no_complete_duplicates/cramers_v_all.txt', header=None)
-print(no_ctx_duplicates)
+counted_ctx_dupicates = count_ctx_duplicates_appearance(no_ctx_duplicates)
+sorted_counted_ctx_dupicates = sorted(counted_ctx_dupicates, key=lambda row: row[1])
+idx=1
+for value in sorted_counted_ctx_dupicates:
+    print('{}) {} - {}'.format(idx, value[0], value[1]))
+    idx += 1
